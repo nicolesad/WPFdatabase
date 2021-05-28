defadb4d58c5b0636059a8de45d883625abbd2b0
nicolesad <32398820+nicolesad@users.noreply.github.com>
2021-05-28 17:46:51 -0600

    Add files via upload

---
+++ Database/DataSet.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Database
+{
+}
+
+
+namespace Database.DataSetTableAdapters {
+
+
+    public partial class SongTableAdapter
+    {
+        internal static void Update()
+        {
+            //SongTableAdapter.Fill(DataSet.);
+        }
+    }
+}
---
+++ Database/MainWindow.xaml.cs
@@ -0,0 +1,42 @@
+﻿using Database.DataSetTableAdapters;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Database
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private  SongTableAdapter adapter = new SongTableAdapter();
+        private  DataSet set = new DataSet();
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Update_Click(object sender, RoutedEventArgs e)
+        {
+            adapter.Update(set);
+        }
+
+        private  void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            adapter.Fill(set.Song);
+            DataContext = set.Song;
+        }
+    }
+}
